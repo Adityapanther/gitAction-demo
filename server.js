3c0389363349fc1c5bcc4a61c9b12cdc13cb6d51
raj68518 <raj68518@gmail.com>
2021-10-17 19:48:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const fastify = require('fastify')({
+    logger: true
+})
+
+const launch = async () => {
+    fastify.get('/', async(req, res) => {
+        res.send({
+            status: "ok",
+            statusCode: 200
+        })
+    })
+    fastify.listen("3000", (err, address) => {
+        console.log(`listenning ${address}`);
+        
+    })
+}
+
+launch()
